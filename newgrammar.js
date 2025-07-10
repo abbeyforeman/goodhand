846444f4c6031efc15be0a6e5989171bc502492e
abbeyforeman <148572488+abbeyforeman@users.noreply.github.com>
2025-07-09 22:38:51 -0400

    Add files via upload

---
+++ newgrammar.js
@@ -0,0 +1,90 @@
+var source =
+
+// paste grammar below!
+
+{
+
+    "me1": ["On #day# #time# when I called you on the phone"],
+    "day": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    "time": ["morning", "afternoon", "evening"],
+
+    "me2": ["I told you about my trip to the #place#"],
+    "place": ["library", "kitchen", "7/11", "subway", "club", "dmv", "grocery store", "post office", "museum", "bus stop", "greenhouse", "gallery", "studio", "movie theater", "gym", "airport", "aquarium", "planetarium", "trampoline park", "rock climbing gym", "home depot"],
+
+    "me3": ["and how I saw the #adj# thing"],
+  "adj": ["funniest", "weirdest", "most peculiar", "strangest", "most bizarre", "wackiest", "zaniest", "most ususual", "most outlandish", "most ridiculous", "most offbeat", "nuttiest", "most kooky", "most bonkers", "most off-the-chain", "most far-out", "whackiest" ],
+
+    "me4": ["On the #ground# there was a #card#"],
+    "ground": ["ground", "wall", "ceiling"],
+    "card": ["6 of hearts", "7 of spades", "8 of clubs", "2 of diamonds", "10 of hearts", "3 of spades", "4 of clubs"],
+
+    "me5": ["Remember that #month# night"],
+    "month": ["January", "February", "March","April","May","June","July","August","September","October","November","December"],
+
+    "me6": ["When we were walking on #streetname# street"],
+    "streetname": ["Benefit", "Smith", "Magnolia","Bluett","Prospect","Power","Arnold","Transit","Greenway","Sugarbush","Benevolent","Water", "Brook","Hope","Angell","Waterman","Ann","Prarie","Nixon","Traver","Baxter"],
+
+    "me7": ["Talking about our favorite things"],
+
+    "me8": ["Like #food# paired with #drink#"],
+    "food": ["shaksuka","bao","persimmons","panna cotta","bahn mi","tempeh","pancakes","sweet potatoes","sushi","fritatta","caprese salad","pasta alla vodka","kimbap","crunchwraps","mac 'n' cheese","chia pudding","soup dumplings","scallion pancakes","fishcake udon","thai curry","pad thai"],
+    "drink": ["mango lassi","coconut water","oat milk", "cashew milk", "kefir","cold brew","oolong tea","a sangria","white wine","red wine","a yerba mate","a korean banana milk","kombucha","chai","aloe juice","carrot juice", "peach tea","plum wine","a gin and tonic","a mojito","a narragansett"],
+
+    "me9": ["Wearing #clothing# with #shoes#"],
+    "clothing": ["skinny jeans","cargo pants","pleated skirts"],
+    "shoes": ["sock boots", "bowling shoes", "platforms", "five-finger shoes", "Birkenstocks","boxing shoes","loafers"],
+
+    "me10": ["Listening to #band# while #activity#"],
+    "band": ["Imagine Dragons", "Cocteau Twins", "Sweet Trip", "Magdalena Bay", "Daft Punk", "Radiohead", "Animal Collective"],
+    "activity": ["jogging", "running errands", "knitting"],
+
+    "me11": ["And our favorite even and odd numbers"],
+    "drank": ["drank", "swallowed", "absorbed", "mimicked", "fused"],
+    "coldness": ["coldness", "fatalism", "terrors", "absence", "shivers", "shadow"],
+
+    "me12": ["Ever since I've seen your favorite number everywhere"],
+
+    "me13": ["On #items2#"],
+    "items2": ["street signs", "movie tickets", "street lamp fliers", "train schedules", "bathroom wall graffiti", "digital billboards", "magazines", "totebags", "door mats", "milk cartons", "egg cartons", "toothpaste tubes", "album art","microwave popcorn bags","laundry detergent", "bus schedules","wine bottles", "coffee mugs","license plates", "gift cards", "water bottles"],
+
+    "me14": ["On #items4#"],
+  "items4": ["street signs", "movie tickets", "street lamp fliers", "train schedules", "bathroom wall graffiti", "digital billboards", "magazines", "totebags", "door mats", "milk cartons", "egg cartons", "toothpaste tubes", "album art","microwave popcorn bags","laundry detergent", "bus schedules","wine bottles", "coffee mugs","license plates", "gift cards", "water bottles"],
+  
+    "me15": ["And even on #items5"],
+    "items5": ["a toaster oven", "a pigeon", "a bicycle", "an umbrella", "a tea kettle", "a monstera leaf", "rubber bands", "a fridge magnet", "a garden gnome", "a toilet plunger", "a corkscrew", "a lint roller"],
+  
+    "me16": ["Maybe your lucky number has become my own"],
+  
+    "me17": ["You respond,"],
+  
+    "me18": ["That's cool but a little pointless"],
+  
+    "me19": ["That wouldn't indicate anything larger"],
+  
+    "me20": ["In the same way the probability of the conception of life doesn't make it uncommon"],
+  
+    "me21": ["Or the side of a coin toss"],
+  
+    "me22": ["Or the amount of #deer# in #state#"],
+  "deer": ["deer","coyotes", "rabbits", "squirrels", "mosquitoes", "woodchucks", "groundhogs"],
+   "state": ["Alaska","Illinois", "Michigan", "Kansas", "Iowa", "California", "Vermont", "Massachusetts", "Oregon", "Minnesota"],
+  
+    "me23": ["I think that there's something beautiful about that though"],
+  
+    "me24": ["Because now I notice when you order #food#"],
+  
+    "me25": ["And remember that #number1# years ago you tried it on accident when the waiter served you the wrong order"],
+    "number1": ["two", "three", "four", "five", "six", "seven"],
+  
+  "me26": ["And now it's your favorite food"],
+  
+    "me27": ["Isn't that serendipitous?"],
+  
+    "me28": ["Like how the right atoms #tickled# in such as way that kicked off the big bang?"],
+  "tickled": ["tickled", "danced around", "bumped up"],
+  
+    "me29": ["You say,"],
+  
+    "me30": ["I guess so"],
+
+}
